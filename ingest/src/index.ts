ca0735ba26fb1021838010ae4f5b0b7fe1d720e8
topweedtw <topweedtw@gmail.com>
2026-06-22 07:01:59 +0000

    feat: fetch url sources into raw storage

--- ingest/src/index.ts
+++ ingest/src/index.ts
@@ -1,4 +1,14 @@
 export {
+  fetchUrlSource,
+  slugFromUrl,
+  storeRawSource,
+  type FetchLike,
+  type FetchUrlSourceOptions,
+  type RawSourceCategory,
+  type RawSourceRecord,
+  type StoredRawSource,
+} from './raw-source.js';
+export {
   appendWikiLogEntry,
   recordWikiWrite,
   upsertWikiIndexEntry,
